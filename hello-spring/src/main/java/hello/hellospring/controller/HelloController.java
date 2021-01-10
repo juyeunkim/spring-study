b940d49c39d13632f4ec67b6ccb2c722c51a720d
juyeunkim <yeun6454@gmail.com>
2021-01-10 16:18:56 +0900

    add requestparam example

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,4 +13,11 @@ public class HelloController {
         model.addAttribute("data", "hello");
         return "hello"; // resources/templates에서 "hello"에 일치하는 html파일을 찾는다
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false)String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
