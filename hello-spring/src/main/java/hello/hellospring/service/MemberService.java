7b406ca485380ecafde95386dcf3af8317390e94
juyeunkim <yeun6454@gmail.com>
2021-01-24 19:19:09 +0900

    자바 코드로 직접 스프링 빈 등록하기
    Config로 Bean 주입

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,12 +9,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+//@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
     // 외부에서 repository를 넣어준다 -- dependency injection
-    @Autowired
+//    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
