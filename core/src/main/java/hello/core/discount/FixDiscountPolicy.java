ac853ccc046faf91a4b747138c4bde38959b7747
juyeunkim <juyeunkim@gmail.com>
2021-10-04 23:34:43 +0900

    @Autowired 필드명, @Qualifier, @Primary

--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,9 +2,12 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+// @Component
+// DiscountPolicy의 하위 타입이 2개이상 빈으로 등록되면 NoUniqueBeanDefinitionException 에러 발생
+// @Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
