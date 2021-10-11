44ae4cb4f142b912a8471e25a5f606e9b2dbaff5
juyeunkim <yeun6454@gmail.com>
2021-10-11 22:59:32 +0900

    빈 생명주기 콜백 시작

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); // 애플리케이션을 닫고싶으면 ConfigurableApplicationContext 사용 ( 기존에는 ApplicationContext 사용 )
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            //// setUrl로 값을 주입하기전에 생성자를 호출했기 때문에 NetworkClient에서 url의 값은 null로 들어간다
+            networkClient.setUrl("http://hello-spring.dev");
+
+            return networkClient;
+        }
+    }
+}
